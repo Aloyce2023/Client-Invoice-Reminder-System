fd892b812bdb5a4666d516d761503aa24f9d8899
Aloyce2023 <aloyce360joseph@gmail.com>
2026-09-09 16:53:45 +0300

    Add changes

--- database.php
+++ database.php
@@ -1,15 +1,15 @@
 <?php
- $conn = mysqli_connect("localhost","root", "","client_invoice_reminder_system");
 
- if($conn)
-    {
-        
-    }
-    else{
-        die("Connection Failed".mysqli_connect_error());
-    }
+$conn = mysqli_connect(
+    "localhost",
+    "root",
+    "",
+    "client_invoice_reminder_system"
+);
 
+if (!$conn) {
+    die("Connection Failed: " . mysqli_connect_error());
+}
 
+mysqli_set_charset($conn, "utf8mb4");
 
-
-?>
